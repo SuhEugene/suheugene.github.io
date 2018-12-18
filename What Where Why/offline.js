9278fcab333bf455fc48d109a3cfb822c28a6fe8
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2018-12-18 22:18:57 +0300

    Upload from questions.json

---
+++ What Where Why/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1545160642,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sydia-sheet0.png",
+		"images/sydia-sheet1.png",
+		"images/sydia-sheet2.png",
+		"images/host-sheet0.png",
+		"images/host-sheet1.png",
+		"images/host-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
