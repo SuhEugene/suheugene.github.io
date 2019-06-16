e71a7eebba10f335f2c501320618f7950ddbe95b
SuhEugene <suheugene@yandex.ru>
2019-06-16 21:16:59 +0300

    test chevr v 0.1-pa

---
+++ precache-manifest.77661dcef2f6b552f0868e9c86d5532e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "072194535002a8d3eeb8",
+    "url": "/static/js/main.07219453.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a518e3e4d53c13c23da5",
+    "url": "/static/css/2.b51a1f08.chunk.css"
+  },
+  {
+    "revision": "a518e3e4d53c13c23da5",
+    "url": "/static/js/2.a518e3e4.chunk.js"
+  },
+  {
+    "revision": "75333bd68e677c7064bc451093136f42",
+    "url": "/index.html"
+  }
+];
